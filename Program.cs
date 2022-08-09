2b93c6bff58b9284644b1bde50da8989aa733728
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-09 20:36:01 +1000

    Код вывода позиций букв введенных пользователем вынесены в функцию и перенесены в файл functions.cs. Добавлена функция строящая виселицу когда пользователь ошибается

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// ИГРА ВИСЕЛИЦА
-
+using functions;
 // ---------------------Объявляем переменные---------------------
 string[] array = {"алмаз", "берег", "горох", "дверь", "живот", "закон", "икота", "кучер"};
 int amountArray = array.Length;
@@ -7,18 +7,19 @@ int randomWordIndex = new Random().Next(1, amountArray);
 string randomWord = array[randomWordIndex];
 char[] arrayUserWord = new char[5];
 int countUserError = 0;
-int maxUserError = 6;
+int maxUserError = 9;
 int countUserWin = 0;
 
 char[] arrayRandomWord = array[randomWordIndex].ToCharArray();
 int arrayRandomWordLength = arrayRandomWord.Length;
 
 // ---------------------Игра---------------------
-Console.WriteLine("Я загадал слово из 5 букв, попробуйте угадать что это за слово, что бы угадать у вас есть 6 попыток. Слово существительное, нарицательное, в единственном числе.");
+Console.WriteLine($"Я загадал слово из 5 букв, попробуйте угадать что это за слово, что бы угадать у вас есть {maxUserError} попыток. Слово существительное, нарицательное, в единственном числе.");
 
 while (countUserError < maxUserError && countUserWin < arrayRandomWordLength) {
     bool userWin = false;
     bool exit = false;
+    Console.WriteLine();
     Console.Write("Введите букву: ");
     char userWord = Console.ReadKey().KeyChar;
     Console.WriteLine();
@@ -48,30 +49,27 @@ while (countUserError < maxUserError && countUserWin < arrayRandomWordLength) {
     }
     // ---------------------Если буква есть показываем какую позицию она занимает---------------------
     if (userWin == true) {
-        i = 0;
-        while (i < arrayUserWord.Length) {
-            if (arrayUserWord[i] != 0) {
-                Console.Write(arrayUserWord[i]);
-            }
-            else {
-                Console.Write("_");
-            }
-            i++;
-        }
-        Console.WriteLine();
+        var userWordClass = new Word();
+        userWordClass.PositionWord(arrayUserWord);
     }
     // ---------------------Если буквы нет---------------------
     if (userWin == false) {
+        Console.WriteLine();
         Console.WriteLine("Нет такой буквы!");
         countUserError++;
+        var err = new Error();
+        err.PrintErr(countUserError);
+        var userWordClass = new Word();
+        userWordClass.PositionWord(arrayUserWord);
     }
     
 }
+// ---------------------Конец игры---------------------
 if (countUserError == maxUserError) {
-    Console.WriteLine($"Вы проиграли, загаданое слово {randomWord}");
+    Console.WriteLine();
+    Console.WriteLine($"Вы проиграли, загаданое слово {randomWord}.");
 }
 if (countUserWin == arrayRandomWordLength) {
-    Console.WriteLine($"Вы победили, загаданое слово {randomWord}");
+    Console.WriteLine();
+    Console.WriteLine($"Вы победили, загаданое слово {randomWord}.");
 }
-
-//Console.WriteLine(arrayRandomWord[0]);d
